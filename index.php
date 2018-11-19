30bd4000cfcf252b128e652a7d49152119924721
Angel <angel.leijendekker@gmail.com>
2018-11-19 15:16:15 +0800

    updated plant and index.html

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<?php  ?>
+
 <!DOCTYPE html>
 <html lang="en" class="top">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Showcase 1</title>
+    <title>Plant Scape</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/fontawesome-all.min.css">
